3fd168485365b8cd3817418a5a12a91d909d566e
Peter De Clercq <dclercqpeter@gmail.com>
2020-10-12 14:16:28 +0200

    Adding Project

---
+++ Assets/Scripts/GrabCoin.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GrabCoin : MonoBehaviour
+{
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindWithTag("Score").GetComponent<Text>().text = score.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Coin")
+        {
+            Destroy(other.gameObject);
+            score += 1;
+            GameObject.FindWithTag("Score").GetComponent<Text>().text = score.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/RocketFlying.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+
+public class RocketFlying : MonoBehaviour
+{
+    public float speed = 20f;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation | RigidbodyConstraints.FreezePositionZ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<Rigidbody>().useGravity = false;
+            transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
+        } else
+        {
+            GetComponent<Rigidbody>().useGravity = true;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, -5));
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 0, 5));
+        }
+
+    }
+}
